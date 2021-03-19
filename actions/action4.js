c2af4dbdebf4d72e8d220515270725f98ec95c80
DataFire <info@datafire.io>
2021-03-19 07:52:11 -0500

    Edit DataFire project

---
+++ actions/action4.js
@@ -0,0 +1,14 @@
+"use strict";
+let datafire = require('datafire');
+
+let gamesparks_game_details = require('@datafire/gamesparks_game_details').actions;
+module.exports = new datafire.Action({
+  id: "action4",
+  handler: async (input, context) => {
+    let segmentQueryFilterConfigModel = await Promise.all([].map(item => gamesparks_game_details.updateSegmentQueryFiltersConfigUsingPUT({
+      segmentQueryConfig: {},
+      apiKey: "",
+    }, context)));
+    return segmentQueryFilterConfigModel;
+  },
+});
